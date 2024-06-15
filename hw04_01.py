e020e06527ebfe81511b0396616d42e44461fc08
Slavik <yaroslav.salko71@gmail.com>
2024-06-15 09:22:49 +0200

    Finish first part for this app

--- hw04_01.py
+++ hw04_01.py
@@ -1,15 +1,22 @@
 from pathlib import Path
 
 def total_salary(path):
+    salary_count = []
     if path.is_file():
         with path.open('r') as file:
             lines = file.readlines()
-            count_salary = 0
             for line in lines:
                 _,salary = line.split(',')
                 salary = int(salary)
-                count_salary += salary
-    
-    return count_salary
+                salary_count.append(salary)
+            
+            sum_salary = sum(salary_count)
+            avg_salary = sum(salary_count) // len(salary_count)
+            min_salary = min(salary_count)
+            max_salary = max(salary_count)
+
+            return f'Загальна сума заробітної плати: {sum_salary},\nСередня заробітна плата: {avg_salary},\nМінімальна заробітня плата: {min_salary},\nМаксимальна заробітня плата: {max_salary}'
+        
+
 path = Path('salary.txt')
-total_salary(path)
+print(total_salary(path))
