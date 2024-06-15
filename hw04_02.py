32864c82772c2293e842fe420377a9bc77de4f0d
Slavik <yaroslav.salko71@gmail.com>
2024-06-15 19:38:00 +0200

    Second task completed

--- hw04_02.py
+++ hw04_02.py
@@ -4,21 +4,40 @@ path = Path('cats.txt')
 
 def get_cats_info(path):
     list_cats = []
+    id, name, age = "", "", 0
     if path.is_file():
         with path.open('r') as file:
             lines = file.readlines()
-            cat_dict = {
-                'id': None,
-                'name' : None,
-                'age' : None
-            }
             for line in lines:
-                id, name, age = line.split(',')
-                cat_dict['id'], cat_dict['name'], cat_dict['age'] = id, name, age
+                id, name, age = line.strip().split(',')
+                cat_dict = {
+                'id': id,
+                'name' : name,
+                'age' : int(age)
+                }
                 list_cats.append(cat_dict)
-            return list_cats
+                   
+        return list_cats
             
     else:
         return f'No such file'
-    
-print(get_cats_info(path))
+
+
+assert(get_cats_info(path)) == [{
+                                'id': '60b90c1c13067a15887e1ae1',
+                                'name' : "Tayson",
+                                'age' : 3
+                                },
+                                {'id': "60b90c2413067a15887e1ae2",
+                                'name' : "Vika",
+                                'age' : 1},
+                                {'id': "60b90c2e13067a15887e1ae3",
+                                'name' : "Barsik",
+                                'age' : 2
+                                },
+                                {'id': "60b90c3b13067a15887e1ae4",
+                                'name' : "Simon",
+                                'age' : 12},
+                                {'id' : '60b90c4613067a15887e1ae5',
+                                'name' : 'Tessi',
+                                'age': 5}]
